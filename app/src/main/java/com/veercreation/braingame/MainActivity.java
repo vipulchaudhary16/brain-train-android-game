abfb05fa5aa77e5c8572e27407243281293eb763
Vipul Chaudhary <77090657+vipulchaudhary16@users.noreply.github.com>
2021-11-20 14:40:20 +0530

    completed

--- app/src/main/java/com/veercreation/braingame/MainActivity.java
+++ app/src/main/java/com/veercreation/braingame/MainActivity.java
@@ -1,4 +1,5 @@
 package com.veercreation.braingame;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
@@ -7,42 +8,52 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Button startBtn, button0, button1, button2, button3 , playAgainButton;
+    Button startBtn, button0, button1, button2, button3, playAgainButton;
     TextView sumTextView, timerText, scoreTextView;
     CountDownTimer countDownTimer;
-    MediaPlayer correctSound, wrongSound , gameOverSound;
+    MediaPlayer correctSound, wrongSound, gameOverSound;
     int a, b, score, numberOfTryPassed = 0;
     ArrayList<Integer> answerList;
     Random rand = new Random();
     int locationForCorrectAnswer;
+    androidx.gridlayout.widget.GridLayout optionGridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         sumTextView = findViewById((R.id.sumText));
         timerText = findViewById(R.id.timerText);
         scoreTextView = findViewById(R.id.scoreText);
-
         startBtn = findViewById(R.id.goButton);
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         playAgainButton = findViewById(R.id.playAgainButton);
+        optionGridLayout = findViewById(R.id.optionGridLayout);
 
         correctSound = MediaPlayer.create(getApplicationContext(), R.raw.success);
         wrongSound = MediaPlayer.create(getApplicationContext(), R.raw.wrong);
         gameOverSound = MediaPlayer.create(getApplicationContext(), R.raw.gameover);
 
-        hideElements();
+        timerText.setVisibility(View.INVISIBLE);
+        sumTextView.setVisibility(View.INVISIBLE);
+        scoreTextView.setVisibility(View.INVISIBLE);
+        optionGridLayout.setVisibility(View.INVISIBLE);
+    }
+
+    public void startGame(View view) {
+        timerText.setVisibility(View.VISIBLE);
+        sumTextView.setVisibility(View.VISIBLE);
+        scoreTextView.setVisibility(View.VISIBLE);
+        optionGridLayout.setVisibility(View.VISIBLE);
+        startBtn.setVisibility(View.INVISIBLE);
         playAgain(scoreTextView);
     }
 
@@ -58,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTextView.setText("0/0");
         timerText.setText("30s");
         generateSum();
-        countDownTimer = new CountDownTimer(11000, 1000) {
+        countDownTimer = new CountDownTimer(31000, 1000) {
             @Override
             public void onTick(long l) {
                 int remainingTime = (int) l / 1000;
@@ -67,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
                     remainingTimeString = "0" + remainingTimeString;
                 timerText.setText(remainingTimeString);
             }
+
             @Override
             public void onFinish() {
                 gameOverSound.start();
@@ -79,15 +91,6 @@ public class MainActivity extends AppCompatActivity {
                 playAgainButton.animate().translationY(1000).setDuration(1000);
             }
         }.start();
-    }
-
-    public void hideElements() {
-
-    }
-
-
-    public void startGame(View view) {
-
     }
 
     public void generateSum() {
@@ -118,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
             correctSound.start();
             score++;
             generateSum();
-        } else if(!(Integer.toString(locationForCorrectAnswer).equals(view.getTag().toString()))){
+        } else if (!(Integer.toString(locationForCorrectAnswer).equals(view.getTag().toString()))) {
             wrongSound.start();
         }
         numberOfTryPassed++;
